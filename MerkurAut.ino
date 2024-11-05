189a82dc95d153047976889e1491aad133f4c69c
MineDragonCZ <MineDragonCZ@email.cz>
2024-11-05 12:19:34 +0100

    Init

--- MerkurAut.ino
+++ MerkurAut.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+Servo myservo;
 
+
+const int MOTORS[] = {10, 9, 6, 5};
+const int LINE_SENSORS[] = {A0, A2};
+const int LINE_SENSORS_TRESHOLD[] = {500, 500};
+
+void setup(){
+  myservo.attach(11);
+  for(int i = 0; i < sizeof(MOTORS); i++){
+    pinMode(MOTORS[i], OUTPUT);
+  }
+  for(int i = 0; i < sizeof(LINE_SENSORS); i++){
+    pinMode(LINE_SENSORS[i], INPUT);
+  }
+
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  // TODO: Write good algorithm to drive the fking car
 }
