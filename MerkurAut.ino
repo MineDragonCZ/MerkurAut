c5d77084100be2275929374a4a2220ced48fd520
MineDragonCZ <MineDragonCZ@email.cz>
2024-11-13 13:08:05 +0100

    Added control for sonar

--- MerkurAut.ino
+++ MerkurAut.ino
@@ -1,34 +1,39 @@
 #include <Servo.h>
-Servo myservo;
+Servo sonarAxisServo;
 
-                    //LF, LB, RF, RB
-const int MOTORS[] = {10, 9, 6, 5};
-const int LINE_SENSORS[] = {A5, A4};
-const int LINE_SENSORS_TRESHOLD[] = {750, 750};
+//LF, LB, RF, RB
+const int MOTORS[] = { 10, 9, 6, 5 };
+const int LINE_SENSORS[] = { A5, A4 };
+const int LINE_SENSORS_TRESHOLD[] = { 750, 750 };
+// Trig, Echo
+const int SONAR_PINS[] = { 12, 13 };
 
 const int motorSpeed = 140;
 
-void setup(){
-  //myservo.attach(11);
-  for(int i = 0; i < sizeof(MOTORS); i++){
+void setup() {
+  //sonarAxisServo.attach(pin serva vole);
+  //sonarAxisServo.write(uhel vole);
+  for (int i = 0; i < sizeof(MOTORS); i++) {
     pinMode(MOTORS[i], OUTPUT);
   }
-  for(int i = 0; i < sizeof(LINE_SENSORS); i++){
+  for (int i = 0; i < sizeof(LINE_SENSORS); i++) {
     pinMode(LINE_SENSORS[i], INPUT);
   }
+  pinMode(SONA_PINS[0], OUTPUT);
+  pinMode(SONA_PINS[1], INPUT);
 
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
   bool middleSensor = testRFSensor(0);
   bool rightSensor = testRFSensor(1);
-  if(middleSensor){
+  if (middleSensor) {
     goForward();
     Serial.println("Forward");
     return;
   }
-  if(rightSensor){
+  if (rightSensor) {
     turnRight();
     Serial.println("Right");
     return;
@@ -44,23 +49,31 @@ bool testRFSensor(int sensorIndex) {
   return (value > LINE_SENSORS_TRESHOLD[sensorIndex]);
 }
 
+double getSonarDistance() {
+  digitalWrite(SONAR_PINS[0], LOW);
+  delayMicroseconds(2);
+  digitalWrite(SONAR_PINS[0], HIGH);
+  delayMicroseconds(5);
+  digitalWrite(SONAR_PINS[0], LOW);
+  double odezva = pulseIn(SONAR_PINS[1], HIGH);
+  return (odezva / 58.31);
+}
+
+
 void turnRight() {
-  for(int i = 0; i < sizeof(MOTORS); i++){
+  for (int i = 0; i < sizeof(MOTORS); i++) {
     analogWrite(MOTORS[i], (!(i == 1 || i == 2) ? motorSpeed : 0));
   }
 }
 
 void turnLeft() {
-  for(int i = 0; i < sizeof(MOTORS); i++){
+  for (int i = 0; i < sizeof(MOTORS); i++) {
     analogWrite(MOTORS[i], ((i == 1 || i == 2) ? motorSpeed : 0));
   }
 }
 
-void goForward(){
-  for(int i = 0; i < sizeof(MOTORS); i++){
+void goForward() {
+  for (int i = 0; i < sizeof(MOTORS); i++) {
     analogWrite(MOTORS[i], (i % 2 == 0 ? motorSpeed : 0));
   }
 }
-
-
-
